d1c42bcf106639beb8afdb1e51438280ae0f99fa
dhananjay-0987 <tdhananjay788@gmail.com>
2025-08-27 11:56:29 +0530

    added model and updated views

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
+class curr_balance(models.Model):
+    balance = models.FloatField(default=0.0)
+    def __str__(self):
+        return str(self.balance)
+
 # Create your models here.
+class track_history(models.Model):
+    balance = models.ForeignKey(curr_balance, on_delete = models.CASCADE)
+    amount = models.FloatField()
+    expense_type = models.CharField(choices = (('Credit', 'Credit'), ('Debit', 'Debit')), max_length=10)
+    description = models.TextField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+
--- tracker/views.py
+++ tracker/views.py
@@ -1,5 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
+from .models import curr_balance, track_history
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
+    if request.method == "POST":
+        description = request.POST.get("description")
+        amount = request.POST.get("amount")
+
+        current_balance, _ = curr_balance.objects.get_or_create(id=1)
+        expense_type = "Credit"
+        if float(amount) < 0:
+            expense_type = "Debit"
+
+        if float(amount) == 0:
+            messages.success(request, "Amount cannot be zero") 
+            return redirect('/')
+        
+        trackhistory = track_history.objects.create(
+            balance=current_balance,
+            amount=amount,
+            expense_type=expense_type,
+            description=description,
+        )
+        current_balance.balance += float(trackhistory.amount)
+        current_balance.save()
+
+        print(description, amount)
+        return redirect("/")
+
     return render(request, "index.html")
