6c1d41a9d2b40cea4e3ddab5fe442e945403b356
dhananjay-0987 <tdhananjay788@gmail.com>
2025-09-02 18:14:58 +0530

    admin panel

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from tracker.models import curr_balance, track_history
 # Register your models here.
+
+admin.site.register(curr_balance)
+
+class TrackHistoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'balance', 'amount', 'expense_type', 'description', 'created_at', 'updated_at')
+    list_filter = ('expense_type', 'created_at')
+    search_fields = ('description',)
+
+
+admin.site.register(track_history, TrackHistoryAdmin)
